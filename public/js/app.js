8615e576dfb693ed222074ad57080ccf1ea7c7a5
DESKTOP-DI4G0T8\terLOOK <terlook52285@gmail.com>
2018-01-16 03:34:43 +0700

    edit img and bug

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-  var socket = io.connect('http://localhost:80');
-  // var socket = io.connect('http://10.1.27.32:80');
+  // var socket = io.connect('http://localhost:80');
+  var socket = io.connect('http://192.168.1.41:80');
 
   // enable vibration support
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
